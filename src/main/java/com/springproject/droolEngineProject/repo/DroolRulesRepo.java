d8ba3e273d2373a273239cc1099c584fc72c85db
Pramod yadav <pramod.yadav@ttecdigital.com>
2025-07-11 12:53:28 +0530

    save rules

--- src/main/java/com/springproject/droolEngineProject/repo/DroolRulesRepo.java
+++ src/main/java/com/springproject/droolEngineProject/repo/DroolRulesRepo.java
@@ -1,6 +1,5 @@
 package com.springproject.droolEngineProject.repo;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.springproject.droolEngineProject.model.Rule;
